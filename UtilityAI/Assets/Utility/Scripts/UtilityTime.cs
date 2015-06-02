2d74b146b8aec74a9257524ef26c01cefae6c272
kabe <sourcexplained@gmail.com>
2015-06-02 10:08:32 +0200

    Modification to UtilityTime to be completely static.

--- UtilityAI/Assets/Utility/Scripts/UtilityTime.cs
+++ UtilityAI/Assets/Utility/Scripts/UtilityTime.cs
@@ -3,14 +3,26 @@ using System.Collections;
 
 public class UtilityTime : MonoBehaviour {
 
-	public static float time;
+	// public static float time;
 	public static float speed = 1.0f;
 	public static bool paused = false;
 
+
+  static public float time {
+    get {
+      if (paused)
+        return 0.0f;
+      else
+        return Time.deltaTime * speed;
+    }
+  }
+
+  /*
 	void Update () {
 		if(!paused)
 			time = Time.deltaTime * speed;
 		else
 			time = 0.0f;
 	}
+   */
 }
